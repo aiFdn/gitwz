1c304cc6efeed0c47b5be09417ade4aa52c1a908
Md. Sazzad Hossain Sharkar <md@szd.sh>
2023-11-15 18:06:35 +0600

    📦 feat: add script to reinstall npm packages with legacy peer deps
    The script `reinstall_npm_packages.sh` is introduced to automate the process of cleaning the npm environment and reinstalling packages. It removes the `package-lock.json` and `node_modules` directory to ensure a fresh installation of dependencies. The `npm cache clean --force` command is used to clear the npm cache, which can prevent potential conflicts or issues with outdated or corrupt cache entries. The `npm install --legacy-peer-deps` command is used to install dependencies while avoiding peer dependency conflicts that may arise with newer npm versions. This script is particularly useful when setting up the project in a new environment or when dependencies need to be reinstalled to resolve conflicts.

---
+++ reinstall_npm_packages.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rm -f package-lock.json && echo "package-lock.json removed" || echo "Failed to remove package-lock.json"
+
+rm -rf node_modules && echo "node_modules removed" || echo "Failed to remove node_modules"
+
+npm cache clean --force && echo "npm cache cleaned" || echo "Failed to clean npm cache"
+
+npm install --legacy-peer-deps && echo "npm install successful" || echo "npm install failed"
