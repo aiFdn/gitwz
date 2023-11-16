863a1d801bd555134305f9e3b653d0f97238797b
Md. Sazzad Hossain Sharkar <md@szd.sh>
2023-11-16 19:10:33 +0600

    feat(utils): update checkIsLatestVersion to display more informative messages The `checkIsLatestVersion` function now displays more informative messages. It provides details about the current and latest versions of GitWiz, along with instructions for updating if a newer version is available. This enhancement improves user experience by providing clear and actionable information about the application's version status.

--- src/utils/checkIsLatestVersion.ts
+++ src/utils/checkIsLatestVersion.ts
@@ -6,20 +6,18 @@ import { getGitWizLatestVersion } from '../api';
 
 export const checkIsLatestVersion = async () => {
     const latestVersion = await getGitWizLatestVersion();
+    const currentVersion = currentPackage.version;
 
-    if (latestVersion) {
-        const currentVersion = currentPackage.version;
-
-        if (currentVersion !== latestVersion) {
-            outro(
-                chalk.yellow(
-                    `
-You are not using the latest stable version of GitWiz with new features and bug fixes.
-Current version: ${currentVersion}. Latest version: ${latestVersion}.
-🚀 To update run: npm i -g gitwz@latest.
-        `,
-                ),
-            );
-        }
+    if (latestVersion && currentVersion !== latestVersion) {
+        outro(
+            `${chalk.bold.blue('INFO:')} You are currently using GitWiz version ${chalk.bold(currentVersion)}. 
+${chalk.bold.green('UPDATE AVAILABLE:')} The latest stable version is ${chalk.bold(latestVersion)}. 
+To update, run: ${chalk.bold.cyan('npm i -g gitwz@latest')}.`,
+        );
+    } else {
+        outro(
+            `${chalk.bold.blue('INFO:')} You are currently using GitWiz version ${chalk.bold(currentVersion)}. 
+This is the latest available version.`,
+        );
     }
 };
