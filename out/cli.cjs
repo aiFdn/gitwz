886625502e51a1c0110c344dec1424476f7388ab
Md. Sazzad Hossain Sharkar <md@szd.sh>
2023-11-16 21:42:53 +0600

    build

--- out/cli.cjs
+++ out/cli.cjs
@@ -23837,7 +23837,7 @@ function G3(t2, e3) {
 // package.json
 var package_default = {
   name: "gitwz",
-  version: "4.0.14",
+  version: "5.0.0",
   packageManager: "^npm@18.0.0",
   description: "Make your git commits stand out! With AI (OpenAI GPT), transform plain git commits into eye-catching ones in just a few seconds.",
   keywords: [
