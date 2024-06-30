17574ca510353664a7ca025441e50b2a7445c243
Md. Sazzad Hossain Sharkar <md@szd.sh>
2024-06-30 14:23:17 +0600

    Build: June 30, 2024, 02:23:17 PM

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,69 +1,69 @@
-import simpleImportSort from "eslint-plugin-simple-import-sort";
-import _import from "eslint-plugin-import";
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import prettier from "eslint-plugin-prettier";
-import { fixupPluginRules } from "@eslint/compat";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+import { fixupPluginRules } from '@eslint/compat';
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import _import from 'eslint-plugin-import';
+import prettier from 'eslint-plugin-prettier';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
     baseDirectory: __dirname,
     recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+    allConfig: js.configs.all,
 });
 
-export default [...compat.extends(
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-), {
-    plugins: {
-        "simple-import-sort": simpleImportSort,
-        import: fixupPluginRules(_import),
-        "@typescript-eslint": typescriptEslint,
-        prettier,
-    },
-
-    languageOptions: {
-        parser: tsParser,
-        ecmaVersion: 2022,
-        sourceType: "module",
-
-        parserOptions: {
-            project: "./tsconfig.json",
+export default [
+    ...compat.extends('eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'),
+    {
+        plugins: {
+            'simple-import-sort': simpleImportSort,
+            import: fixupPluginRules(_import),
+            '@typescript-eslint': typescriptEslint,
+            prettier,
         },
-    },
 
-    settings: {
-        "import/resolver": {
-            node: {
-                extensions: [".js", ".jsx", ".ts", ".tsx"],
+        languageOptions: {
+            parser: tsParser,
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+
+            parserOptions: {
+                project: './tsconfig.json',
             },
         },
-    },
 
-    rules: {
-        "prettier/prettier": "error",
-        "no-console": "error",
-        "import/order": "off",
-        "sort-imports": "off",
-        "simple-import-sort/imports": "error",
-        "simple-import-sort/exports": "error",
-        "import/first": "error",
-        "import/newline-after-import": "error",
-        "import/no-duplicates": "error",
-        "@typescript-eslint/no-non-null-assertion": "off",
-        "@typescript-eslint/no-explicit-any": "off",
-        complexity: ["warn", 10],
-        "max-depth": ["warn", 4],
-        "@typescript-eslint/consistent-type-definitions": ["error", "type"],
-        "@typescript-eslint/prefer-for-of": "warn",
-        "@typescript-eslint/prefer-includes": "warn",
-        "@typescript-eslint/prefer-string-starts-ends-with": "warn",
+        settings: {
+            'import/resolver': {
+                node: {
+                    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+                },
+            },
+        },
+
+        rules: {
+            'prettier/prettier': 'error',
+            'no-console': 'error',
+            'import/order': 'off',
+            'sort-imports': 'off',
+            'simple-import-sort/imports': 'error',
+            'simple-import-sort/exports': 'error',
+            'import/first': 'error',
+            'import/newline-after-import': 'error',
+            'import/no-duplicates': 'error',
+            '@typescript-eslint/no-non-null-assertion': 'off',
+            '@typescript-eslint/no-explicit-any': 'off',
+            complexity: ['warn', 10],
+            'max-depth': ['warn', 4],
+            '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+            '@typescript-eslint/prefer-for-of': 'warn',
+            '@typescript-eslint/prefer-includes': 'warn',
+            '@typescript-eslint/prefer-string-starts-ends-with': 'warn',
+        },
     },
-}];
+];
